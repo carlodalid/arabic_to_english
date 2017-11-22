a3236aea57a5077a86efba9dccab8e18143ee113
Carl Dalid <carlodalid@gmail.com>
2017-11-23 00:09:22 +0800

    Add test cases for four digit numbers

--- spec/arabic2english_spec.rb
+++ spec/arabic2english_spec.rb
@@ -38,6 +38,30 @@ describe 'Arabic2English' do
     it 'should translate 888 to eight hundred eighty eight' do
       expect { Arabic2English.translate(888) }.to output("eight hundred eighty eight\n").to_stdout
     end
+
+    it 'should translate 1000 to one thousand' do
+      expect { Arabic2English.translate(1000) }.to output("one thousand\n").to_stdout
+    end
+
+    it 'should translate 1001 to one thousand one' do
+      expect { Arabic2English.translate(1001) }.to output("one thousand one\n").to_stdout
+    end
+
+    it 'should translate 1020 to one thousand twenty' do
+      expect { Arabic2English.translate(1020) }.to output("one thousand twenty\n").to_stdout
+    end
+
+    it 'should translate 1033 to one thousand thirty three' do
+      expect { Arabic2English.translate(1033) }.to output("one thousand thirty three\n").to_stdout
+    end
+
+    it 'should translate 1770 to one thousand seven hundred seventy' do
+      expect { Arabic2English.translate(1770) }.to output("one thousand seven hundred seventy\n").to_stdout
+    end
+
+    it 'should translate 1900 to one thousand nind hundred' do
+      expect { Arabic2English.translate(1900) }.to output("one thousand nine hundred\n").to_stdout
+    end
   end
 
   describe 'when given an invalid input' do
