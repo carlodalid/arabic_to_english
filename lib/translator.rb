0f1653ef670597d0b6a6e32c6317b786a2e9e175
Carl Dalid <carlodalid@gmail.com>
2017-11-22 23:37:22 +0800

    Add method to translate two digits

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,31 @@ module Translator
     'five', 'six', 'seven', 'eight', 'nine'
   ]
 
+  TYS = [
+    'zero', 'ten', 'twenty', 'thirty', 'forty',
+    'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+  ]
+
   def self.translate(param)
-    ONES[param]
+    num_str = param.to_s
+    num_arr = num_str.split('')
+
+    case num_arr.length
+    when 1
+      ONES[param]
+    when 2
+      translate_two_digits(num_arr)
+    end
+  end
+
+  private
+
+  def self.translate_two_digits(param)
+    ones = param[1].to_i
+    tens = param[0].to_i
+
+    if ones === 0
+      "#{TYS[tens]}"
+    end
   end
 end
